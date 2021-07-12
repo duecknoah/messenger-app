f1a98c1e4fac2284ac6a440993fdd0f412e72730
duecknoah <duecknoah@gmail.com>
2021-07-12 14:41:04 -0700

    Fixes message ordering issue
    Message ordering was originally fixed by changing
    display order of elements. However if a new
    message came in, it would appear at the top.
    
    Resolved by reversing order of messages in the backend
    instead, that way new messages would be handled no differently
    in the front-end

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,9 +8,7 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.map((val, index, arr) => {
-        // Reverse message ordering
-        const message = arr[arr.length - 1 - index];
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
